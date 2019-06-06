1deef685f77959df31aec6a04f1464abc716b76e
Santiago Enciso <whereispolaris@gmail.com>
2019-06-05 19:20:07 -0500

    Added columns and rows to container div,  created variables and array for game.

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,68 @@
-console.log("testing js file")
+var selectedCountry;
+var wins;
+var losses;
+var lettersAlreadyGuessed;
+
+var countries = [
+    "china" ,
+    "india" ,
+    "united states" ,
+    "indonesia" ,
+    "brazil" ,
+    "pakistan" ,
+    "nigeria" ,
+    "bangladesh" ,
+    "russia" ,
+    "japan" ,
+    "mexico" ,
+    "philippines" ,
+    "ethiopia" ,
+    "vietnam" ,
+    "egypt" ,
+    "iran" ,
+    "congo" ,
+    "germany" ,
+    "turkey" ,
+    "thailand" ,
+    "france" ,
+    "united kingdom" ,
+    "italy" ,
+    "burma" ,
+    "south africa" ,
+    "tanzania" ,
+    "korea, south" ,
+    "spain" ,
+    "colombia" ,
+    "kenya" ,
+    "ukraine" ,
+    "argentina" ,
+    "algeria" ,
+    "poland" ,
+    "uganda" ,
+    "iraq" ,
+    "sudan" ,
+    "canada" ,
+    "morocco" ,
+    "afghanistan" ,
+    "malaysia" ,
+    "venezuela" ,
+    "peru" ,
+    "uzbekistan" ,
+    "nepal" ,
+    "saudi arabia" ,
+    "yemen" ,
+    "ghana" ,
+    "mozambique" ,
+    "north korea" ,
+]
+
+
+
+// Loop:
+// for (var i = 0; i < countries.length; i++) {
+//     console.log(countries[i].toLowerCase());
+// }
+
+// Get random number:
+// Math.floor(Math.random() * countries.length);
+
