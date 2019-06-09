1f8302dfe776319a934cf8a3b627e9d1462dcbaf
Santiago Enciso <whereispolaris@gmail.com>
2019-06-09 12:55:58 -0500

    Organized code in order of importance. Added loop that appends span elements with letters and classes.

--- assets/js/game.js
+++ assets/js/game.js
@@ -12,28 +12,56 @@ var guessesLeft = 12;
 var wordDisplay; // what renders on the index.html file right next to WORD:
 var getWord = document.getElementById("appendWord"); // targets appendWord ID.
 
-// CREATE FUNCTIONS 
+ // Array of countries (REMOVE ARRAYS WITH TWO WORDS)
+var countries = ["china" , "india" , "indonesia" , "brazil" , "pakistan" , "nigeria" , "bangladesh" , "russia" , "japan" , "mexico" , "philippines" , "ethiopia" , "vietnam" , "egypt" , "iran" , "congo" , "germany" , "turkey" , "thailand" , "france" , "italy" , "burma", "tanzania" , "panama" , "spain" , "colombia" , "kenya" , "ukraine" , "argentina" , "algeria" , "poland" , "uganda" , "iraq" , "sudan" , "canada" , "morocco" , "afghanistan" , "malaysia" , "venezuela" , "peru" , "uzbekistan" , "nepal", "yemen" , "ghana" , "mozambique"];
 
-// CREATE AN ARRAY FOR THE LETTERS
+// Array of letters
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+// Countries that have been chosen already
 var usedCountries = [];
 
- // Create array of countries (REMOVE ARRAYS WITH TWO WORDS)
-var countries = ["china" , "india" , "indonesia" , "brazil" , "pakistan" , "nigeria" , "bangladesh" , "russia" , "japan" , "mexico" , "philippines" , "ethiopia" , "vietnam" , "egypt" , "iran" , "congo" , "germany" , "turkey" , "thailand" , "france" , "italy" , "burma", "tanzania" , "korea, south" , "spain" , "colombia" , "kenya" , "ukraine" , "argentina" , "algeria" , "poland" , "uganda" , "iraq" , "sudan" , "canada" , "morocco" , "afghanistan" , "malaysia" , "venezuela" , "peru" , "uzbekistan" , "nepal", "yemen" , "ghana" , "mozambique"];
-
+// Letters that have been chosen already
+var usedLetters = [];
 
 // CHOOSE RANDOM OBJECT FROM countries[] ARRAY
 var selectedCountry = countries[Math.floor(Math.random() * countries.length)];
 console.log("The random country is " + selectedCountry);
+
+// Testing one Single country
+selectedCountry = "colombia";
+
+// Break down the selectedWord into individual letters:
+wordLetters = Array.from(selectedCountry);
+console.log(wordLetters);
+
+// Run loop that creates span element with class="LETTER" and appends to <h2 class="wordDisplay"> element
+for (var i = 0; i < wordLetters.length; i++) {
+    $("#wordDisplay").append('<span class="' + wordLetters[i] + ' notGuessed">' + wordLetters[i] + '</span>');
+    // console.log(spanElement);
+       //ADD CLASS  
+}
+
+// document.write(spanElement);
+
+console.log(wordLetters[0]);
+
+// ===== ALTERNATIVE LOOP =======
+// wordLetters.forEach( function(element){
+//     console.log(element);
+//     var spanElement = document.createElement("span");
+//     var spanContent = document.createTextNode(element);
+//     spanElement.appendChild(spanContent);
+//     document.write(spanElement);
+//     //ADD CLASS
+// });
 
 // Count letters in selectedCountry
 var letterCount = selectedCountry.length;
 console.log(letterCount );
 
 
-// Break down the selectedWord into individual letters:
-wordLetters = Array.from(selectedCountry);
-console.log(wordLetters);
+
 
 // for every characted in letterCount, add underscode to h1 span 
 
@@ -42,8 +70,8 @@ console.log(wordLetters);
 
 
 // Ask user to guess letter
-var guessedLetter = prompt("Choose letter");
-console.log("you chose " + guessedLetter);
+// var guessedLetter = prompt("Choose letter").toLowerCase();
+// console.log("you chose " + guessedLetter);
 
 // find a way to see if letter is present in selectedCountry
 
