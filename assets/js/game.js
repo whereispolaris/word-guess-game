3a1c78fe389667c134cf17b0c2f2066b1a79d344
Santiago Enciso <whereispolaris@gmail.com>
2019-06-09 11:20:55 -0500

    Removed redundant comments, added array of letters, insterted function that breaks down string int array of letters.

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,46 +3,53 @@
 
 // Create fundamental variables
 var selectedCountry;
+var wordLetters;
 var guessedLetter;
 var wins = 0;
 var losses = 0;
 var lettersAlreadyGuessed = [];
-var guessesLeft;
+var guessesLeft = 12;
+var wordDisplay; // what renders on the index.html file right next to WORD:
+var getWord = document.getElementById("appendWord"); // targets appendWord ID.
 
- // Create array of countries
-var countries = ["china" , "india" , "united states" , "indonesia" , "brazil" , "pakistan" , "nigeria" , "bangladesh" , "russia" , "japan" , "mexico" , "philippines" , "ethiopia" , "vietnam" , "egypt" , "iran" , "congo" , "germany" , "turkey" , "thailand" , "france" , "united kingdom" , "italy" , "burma" , "south africa" , "tanzania" , "korea, south" , "spain" , "colombia" , "kenya" , "ukraine" , "argentina" , "algeria" , "poland" , "uganda" , "iraq" , "sudan" , "canada" , "morocco" , "afghanistan" , "malaysia" , "venezuela" , "peru" , "uzbekistan" , "nepal" , "saudi arabia" , "yemen" , "ghana" , "mozambique" , "north korea"]
+// CREATE FUNCTIONS 
+
+// CREATE AN ARRAY FOR THE LETTERS
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var usedCountries = [];
+
+ // Create array of countries (REMOVE ARRAYS WITH TWO WORDS)
+var countries = ["china" , "india" , "indonesia" , "brazil" , "pakistan" , "nigeria" , "bangladesh" , "russia" , "japan" , "mexico" , "philippines" , "ethiopia" , "vietnam" , "egypt" , "iran" , "congo" , "germany" , "turkey" , "thailand" , "france" , "italy" , "burma", "tanzania" , "korea, south" , "spain" , "colombia" , "kenya" , "ukraine" , "argentina" , "algeria" , "poland" , "uganda" , "iraq" , "sudan" , "canada" , "morocco" , "afghanistan" , "malaysia" , "venezuela" , "peru" , "uzbekistan" , "nepal", "yemen" , "ghana" , "mozambique"];
 
 
 // CHOOSE RANDOM OBJECT FROM countries[] ARRAY
 var selectedCountry = countries[Math.floor(Math.random() * countries.length)];
-console.log(selectedCountry);
+console.log("The random country is " + selectedCountry);
 
 // Count letters in selectedCountry
-var letterCount = selectedCountry.valueOf();
-console.log(letterCount);
-console.log(letterCount.length);
+var letterCount = selectedCountry.length;
+console.log(letterCount );
+
+
+// Break down the selectedWord into individual letters:
+wordLetters = Array.from(selectedCountry);
+console.log(wordLetters);
 
 // for every characted in letterCount, add underscode to h1 span 
+
 // e.g "colombia" will be printed as _ _ _ _ _ _ _ _
-
-//find a way to see if letter is present in selectedCountry
-
-// if letter matches selectedCountry, add it to Word 
-    //find position of letter in string
-    // Remove underscore(s) and replace with letters
-    // e.g guessedLetter = "o"; will update Word to _ o _ o _ _ _ _
-
-// if letter does not  match
-    // Remove one guessesLeft;
-
-// // Append selectedCountry to Word: h1 span - THIS IS TEMPORARY
-var getWord = document.getElementById("appendWord");
-getWord.innerHTML = " " + selectedCountry;
+    // run a loop that renders html tag that will display blanks (_)
 
 
 // Ask user to guess letter
 var guessedLetter = prompt("Choose letter");
-console.log(guessedLetter);
+console.log("you chose " + guessedLetter);
+
+// find a way to see if letter is present in selectedCountry
 
 
-// console.log(guessedLetter.includes(selectedCountry)); // false
+
+// if letter matches selectedCountry, add it to wordDisplay
+
+// if letter does not  match
+    // Remove one guessesLeft;
